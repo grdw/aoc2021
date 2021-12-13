0f2be7f7e08695f821198f32b36ca45622819858
grdw <gerard@wetransfer.com>
2021-12-13 10:29:15 +0100

    Rewrite double visit

--- problem_0012/src/main.rs
+++ problem_0012/src/main.rs
@@ -50,40 +50,54 @@ struct CaveSystem<'a> {
     map: HashMap<Cave<'a>, Vec<Cave<'a>>>
 }
 
-//fn double_visit(route: &Vec<&str>, neighbor: &Cave) -> bool {
-//    let mut counts = HashMap::new();
-//
-//    for c in route {
-//        if big_cave(c) || c == &"start" {
-//            continue
-//        }
-//
-//        let counter = counts.entry(c).or_insert(0);
-//        *counter += 1;
-//    }
-//
-//    let counter = counts.entry(&neighbor).or_insert(0);
-//    *counter += 1;
-//
-//    let two_counts = counts.values().filter(|&&n| n >= 2).count();
-//    let neighbor_count = counts.get(&neighbor).unwrap() - 1;
-//
-//    two_counts < 2 && neighbor_count < 2
-//}
-//
-//#[test]
-//fn test_double_visit() {
-//    let route = vec!["start"];
-//    assert!(double_visit(&route, "c"));
-//
-//    let route = vec!["start", "c", "c", "D", "b"];
-//    assert!(!double_visit(&route, "c"));
-//    assert!(!double_visit(&route, "b"));
-//    assert!(double_visit(&route, "a"));
-//
-//    let route = vec!["start", "c", "D", "b"];
-//    assert!(double_visit(&route, "c"));
-//}
+fn double_visit(route: &Vec<&Cave>, neighbor: &Cave) -> bool {
+    let mut counts = HashMap::new();
+
+    for c in route {
+        match c {
+            Cave::Small(_) => {
+                let counter = counts.entry(c).or_insert(0);
+                *counter += 1;
+            },
+            _ => continue
+        }
+    }
+
+    let counter = counts.entry(&neighbor).or_insert(0);
+    *counter += 1;
+
+    let two_counts = counts.values().filter(|&&n| n >= 2).count();
+    let neighbor_count = counts.get(&neighbor).unwrap() - 1;
+
+    two_counts < 2 && neighbor_count < 2
+}
+
+#[test]
+fn test_double_visit() {
+    let route = vec![&Cave::Start];
+    assert!(double_visit(&route, &Cave::Small("c")));
+
+    let route = vec![
+        &Cave::Start,
+        &Cave::Small("c"),
+        &Cave::Small("c"),
+        &Cave::Big("D"),
+        &Cave::Small("b")
+    ];
+
+    assert!(!double_visit(&route, &Cave::Small("c")));
+    assert!(!double_visit(&route, &Cave::Small("b")));
+    assert!(double_visit(&route, &Cave::Small("a")));
+
+    let route = vec![
+        &Cave::Start,
+        &Cave::Small("c"),
+        &Cave::Big("D"),
+        &Cave::Small("b")
+    ];
+
+    assert!(double_visit(&route, &Cave::Small("c")));
+}
 
 impl CaveSystem<'_> {
     fn from_vec<'a>(input: &'a Vec<&str>) -> CaveSystem<'a> {
@@ -158,32 +172,39 @@ impl CaveSystem<'_> {
         routes
     }
 
-    //fn double_count_paths(&self, start: &Cave) -> usize {
-    //    let mut to_visit = vec![];
-    //    let mut routes = 0;
+    fn double_count_paths(&self) -> usize {
+        let mut to_visit = vec![];
+        let mut routes = 0;
 
-    //    to_visit.push(vec![start]);
+        to_visit.push(vec![&Cave::Start]);
 
-    //    while let Some(route) = to_visit.pop() {
-    //        let current = route[route.len() - 1];
+        while let Some(route) = to_visit.pop() {
+            let current = route[route.len() - 1];
 
-    //        if Cave::End == current {
-    //            routes += 1;
-    //        }
+            if &Cave::End == current {
+                routes += 1;
+            }
 
-    //        if let Some(neighbors) = self.map.get(current) {
-    //            for neighbor in neighbors {
-    //                if big_cave(neighbor) || double_visit(&route, neighbor) {
-    //                    let mut new_route = route.clone();
-    //                    new_route.push(neighbor);
-    //                    to_visit.push(new_route.clone());
-    //                }
-    //            }
-    //        }
-    //    }
+            if let Some(neighbors) = self.map.get(current) {
+                for neighbor in neighbors {
+                    let mut new_route = route.clone();
 
-    //    routes
-    //}
+                    let condition = match neighbor {
+                        &Cave::Big(_) | &Cave::End => true,
+                        &Cave::Small(_) => double_visit(&route, neighbor),
+                        _ => false
+                    };
+
+                    if condition {
+                        new_route.push(neighbor);
+                        to_visit.push(new_route.clone());
+                    }
+                }
+            }
+        }
+
+        routes
+    }
 }
 
 #[test]
@@ -213,54 +234,72 @@ fn test_passage_pathing_example() {
     );
     assert_eq!(system.map.get(&Cave::End), None);
     assert_eq!(system.count_paths(), 10);
-    //assert_eq!(system.double_count_paths("start"), 36);
+    assert_eq!(system.double_count_paths(), 36);
 }
 
-//#[test]
-//fn test_passage_pathing_complex_example() {
-//    let other_example = vec![
-//        "dc-end",
-//        "HN-start",
-//        "start-kj",
-//        "dc-start",
-//        "dc-HN",
-//        "LN-dc",
-//        "HN-end",
-//        "kj-sa",
-//        "kj-HN",
-//        "kj-dc"
-//    ];
-//
-//    let system = CaveSystem::from_vec(&other_example);
-//    assert_eq!(system.map.get("start"), Some(&vec!["HN", "kj", "dc"]));
-//    assert_eq!(system.map.get("dc"), Some(&vec!["end", "HN", "LN", "kj"]));
-//    assert_eq!(system.map.get("end"), None);
-//    assert_eq!(system.count_paths("start"), 19);
-//}
-//
-//#[test]
-//fn test_passage_pathing_more_complex_example() {
-//    let complex_example = vec![
-//        "fs-end",
-//        "he-DX",
-//        "fs-he",
-//        "start-DX",
-//        "pj-DX",
-//        "end-zg",
-//        "zg-sl",
-//        "zg-pj",
-//        "pj-he",
-//        "RW-he",
-//        "fs-DX",
-//        "pj-RW",
-//        "zg-RW",
-//        "start-pj",
-//        "he-WI",
-//        "zg-he",
-//        "pj-fs",
-//        "start-RW"
-//    ];
-//
-//    let system = CaveSystem::from_vec(&complex_example);
-//    assert_eq!(system.count_paths("start"), 226);
-//}
+#[test]
+fn test_passage_pathing_complex_example() {
+    let other_example = vec![
+        "dc-end",
+        "HN-start",
+        "start-kj",
+        "dc-start",
+        "dc-HN",
+        "LN-dc",
+        "HN-end",
+        "kj-sa",
+        "kj-HN",
+        "kj-dc"
+    ];
+
+    let system = CaveSystem::from_vec(&other_example);
+    assert_eq!(
+        system.map.get(&Cave::Start),
+        Some(&vec![
+             Cave::Big("HN"),
+             Cave::Small("kj"),
+             Cave::Small("dc")
+        ])
+    );
+    assert_eq!(
+        system.map.get(&Cave::Small("dc")),
+        Some(&vec![
+             Cave::End,
+             Cave::Big("HN"),
+             Cave::Big("LN"),
+             Cave::Small("kj")
+        ])
+    );
+    assert_eq!(
+        system.map.get(&Cave::End),
+        None
+    );
+    assert_eq!(system.count_paths(), 19);
+}
+
+#[test]
+fn test_passage_pathing_more_complex_example() {
+    let complex_example = vec![
+        "fs-end",
+        "he-DX",
+        "fs-he",
+        "start-DX",
+        "pj-DX",
+        "end-zg",
+        "zg-sl",
+        "zg-pj",
+        "pj-he",
+        "RW-he",
+        "fs-DX",
+        "pj-RW",
+        "zg-RW",
+        "start-pj",
+        "he-WI",
+        "zg-he",
+        "pj-fs",
+        "start-RW"
+    ];
+
+    let system = CaveSystem::from_vec(&complex_example);
+    assert_eq!(system.count_paths(), 226);
+}
