b48c6a48678979bd64b3ff25c175a2f5e27f431a
grdw <gerard@wetransfer.com>
2021-12-13 16:52:01 +0100

    Turn points into a HashSet to reduce duplicate checking

--- problem_0013/src/main.rs
+++ problem_0013/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
+use std::collections::HashSet;
 
-type Points = Vec<(usize, usize)>;
+type Points = HashSet<(usize, usize)>;
 type Folds<'a> = Vec<(&'a str, usize)>;
 
 fn main() {
@@ -10,17 +11,16 @@ fn main() {
     let folds = fs::read_to_string("folds")
                    .unwrap_or("".to_string());
 
-    let points: Points = input
-        .split_terminator("\n")
-        .map(|line| {
-             let yx: Vec<usize> = line
-                .split(",")
-                .map(|t| t.parse::<usize>().unwrap())
-                .collect();
+    let mut points: Points = HashSet::new();
 
-             (yx[0], yx[1])
-        })
-        .collect();
+    for line in input.split_terminator("\n") {
+         let yx: Vec<usize> = line
+            .split(",")
+            .map(|t| t.parse::<usize>().unwrap())
+            .collect();
+
+         points.insert((yx[0], yx[1]));
+    }
 
     let folds: Folds = folds
         .split_terminator("\n")
@@ -62,39 +62,36 @@ fn fold_paper(points: &mut Points, folds: &Folds, times: usize) {
         let height = points.iter().map(|n| n.1).max().unwrap();
         let width = points.iter().map(|n| n.0).max().unwrap();
 
-        let mut folds: Points = vec![];
-        let mut unfolds: Points = vec![];
+        let mut folds: Points = HashSet::new();
+        let mut unfolds: Points = HashSet::new();
 
-        for i in (0..points.len()).rev() {
-            let (x, y) = points[i];
-            let val = if axis == "y" { y } else { x };
+        for (x, y) in points.iter() {
+            let val = if axis == "y" { *y } else { *x };
 
             if val > value {
-                folds.push((x, y));
-                points.remove(i);
+                folds.insert((*x, *y));
             } else if val < value {
-                unfolds.push((x, y));
+                unfolds.insert((*x, *y));
             }
         }
 
-        for i in 0..folds.len() {
-            let (x, y) = folds[i];
+        for (x, y) in folds {
+            points.remove(&(x, y));
+
             let folded_point = if axis == "y" {
                 (x, height - y)
             } else {
                 (width - x, y)
             };
 
-            if !points.contains(&folded_point) {
-                points.push(folded_point)
-            }
+            points.insert(folded_point);
         }
     }
 }
 
 #[test]
 fn test_transparent_origami() {
-    let points = vec![
+    let input = vec![
         (6,10),
         (0,14),
         (9,10),
@@ -114,6 +111,11 @@ fn test_transparent_origami() {
         (8,10),
         (9,0),
     ];
+
+    let mut points = HashSet::new();
+    for p in input {
+        points.insert(p);
+    }
 
     let folds = vec![
         ("y", 7),
