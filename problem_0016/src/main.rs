b970257cb6f6c0c666594ada1f1a0185735b3eda
grdw <gerard@grdw.nl>
2021-12-16 20:33:34 +0100

    resolve part 1

--- problem_0016/src/main.rs
+++ problem_0016/src/main.rs
@@ -1,15 +1,20 @@
 use std::fs;
+use std::str;
+use std::io::prelude::*;
+use std::io::Cursor;
 
 fn main() {
     let display_string = fs::read_to_string("input")
                             .unwrap_or("".to_string());
 
     let bytes = display_string.as_bytes();
-    let binary = bytes_to_bin(&bytes);
-    parse(&binary);
+    let mut cursor = bytes_to_bin(&bytes);
+    let mut counter = 0;
+    parse(&mut cursor, &mut counter);
+    println!("Part 1: {}", counter);
 }
 
-fn bytes_to_bin(bytes: &[u8]) -> String {
+fn bytes_to_bin(bytes: &[u8]) -> Cursor<String> {
     let mut start = String::from("");
     for b in bytes {
         if b < &11 { continue };
@@ -18,61 +23,152 @@ fn bytes_to_bin(bytes: &[u8]) -> String {
         let binary = format!("{:04b}", int);
         start.push_str(&binary);
     }
-    start
+    Cursor::new(start)
 }
 
 #[test]
 fn test_bytes_to_bin() {
     let bytes = "D2FE28".as_bytes();
-    let bin = bytes_to_bin(&bytes);
+    let mut cursor = bytes_to_bin(&bytes);
+    let mut bin = String::new();
+    let _read_bytes = cursor.read_to_string(&mut bin);
 
     assert_eq!(bin, "110100101111111000101000");
 }
 
-fn parse(binary: &String) {
-    let version = u64::from_str_radix(&binary[0..3], 2).unwrap();
-    let type_id = u64::from_str_radix(&binary[3..6], 2).unwrap();
-    let type_len_id = binary.chars().nth(7).unwrap();
-    let is_literal = type_id == 4;
+fn read_ahead(cursor: &mut Cursor<String>, bytes: usize) -> u64 {
+    let mut buf = vec![0; bytes];
+    let _c = cursor.read_exact(&mut buf);
+    let version = str::from_utf8(&buf).unwrap();
+    u64::from_str_radix(&version, 2).unwrap()
+}
 
-    if is_literal {
-        let mut count = 0;
-        let group_size = 5;
-        let mut binar_rep = String::from("");
+fn read_literal_value(cursor: &mut Cursor<String>) -> u64 {
+    let mut total = String::new();
+    loop {
+        let mut buf = vec![0; 5];
+        let _c = cursor.read_exact(&mut buf);
+        let version = str::from_utf8(&buf).unwrap();
 
-        loop {
-            let start = 6 + (count * group_size);
-            let end = start + group_size;
-            let blob = &binary[start..end];
+        total.push_str(&version[1..5]);
 
-            binar_rep.push_str(&blob[1..group_size]);
-            count += 1;
-
-            if &blob[..1] == "0" {
-                break;
-            }
+        if &version[..1] == "0" {
+            break;
         }
-
-        println!(
-            "{} {}",
-             binar_rep,
-             u64::from_str_radix(&binar_rep, 2).unwrap()
-        );
-    } else if type_len_id == '0' {
-        let total_l = u64::from_str_radix(&binary[7..22], 2).unwrap();
-        println!("{}", total_l);
-    } else if type_len_id == '1' {
-        let num_of_sub = u64::from_str_radix(&binary[6..17], 2).unwrap();
-        println!("{}", num_of_sub);
     }
-    println!("");
+    u64::from_str_radix(&total, 2).unwrap()
+}
+
+fn parse(cursor: &mut Cursor<String>, counter: &mut u64) {
+    let version = read_ahead(cursor, 3);
+    let type_id = read_ahead(cursor, 3);
+
+    *counter += version;
+
+    if type_id == 4 {
+        let _value = read_literal_value(cursor);
+    } else {
+        let type_length_id = read_ahead(cursor, 1);
+
+        if type_length_id == 0 {
+            let total_length = read_ahead(cursor, 15);
+            parse_with_read_limit(cursor, total_length, counter);
+        } else if type_length_id == 1 {
+            let number_of_packs = read_ahead(cursor, 11);
+            parse_with_packet_limit(cursor, number_of_packs, counter);
+        }
+    }
+}
+
+fn parse_with_read_limit(
+    cursor: &mut Cursor<String>,
+    limit: u64,
+    counter: &mut u64
+) {
+    let curr_poss = cursor.position();
+    let limit = curr_poss + limit;
+
+    while cursor.position() < limit {
+        parse(cursor, counter);
+    }
+}
+
+fn parse_with_packet_limit(
+    cursor: &mut Cursor<String>,
+    limit: u64,
+    counter: &mut u64
+) {
+    let mut count = 0;
+
+    while count < limit {
+        parse(cursor, counter);
+        count += 1;
+    }
 }
 
 #[test]
-fn test_parse_bytes() {
+fn test_parse_bytes_literal_value() {
     let bytes = "D2FE28".as_bytes();
-    let bin = bytes_to_bin(&bytes);
-    parse(&bin);
+    let mut cursor = bytes_to_bin(&bytes);
+    let mut counter = 0;
+    parse(&mut cursor, &mut counter);
 
-    assert_eq!(bin, "110100101111111000101000");
+    cursor.set_position(0);
+    let mut full_read = String::new();
+    let _bytes_read = cursor.read_to_string(&mut full_read);
+
+    assert_eq!(full_read, "110100101111111000101000");
+}
+
+
+#[test]
+fn test_parse_bytes_type_0() {
+    let bytes = "38006F45291200".as_bytes();
+    let mut cursor = bytes_to_bin(&bytes);
+    let mut counter = 0;
+    parse(&mut cursor, &mut counter);
+}
+
+#[test]
+fn test_parse_bytes_type_1() {
+    let bytes = "EE00D40C823060".as_bytes();
+    let mut cursor = bytes_to_bin(&bytes);
+    let mut counter = 0;
+    parse(&mut cursor, &mut counter);
+}
+
+#[test]
+fn test_parse_complex_1() {
+    let bytes = "8A004A801A8002F478".as_bytes();
+    let mut cursor = bytes_to_bin(&bytes);
+    let mut counter = 0;
+    parse(&mut cursor, &mut counter);
+    assert_eq!(counter, 16);
+}
+
+#[test]
+fn test_parse_complex_2() {
+    let bytes = "620080001611562C8802118E34".as_bytes();
+    let mut cursor = bytes_to_bin(&bytes);
+    let mut counter = 0;
+    parse(&mut cursor, &mut counter);
+    assert_eq!(counter, 12);
+}
+
+#[test]
+fn test_parse_complex_3() {
+    let bytes = "C0015000016115A2E0802F182340".as_bytes();
+    let mut cursor = bytes_to_bin(&bytes);
+    let mut counter = 0;
+    parse(&mut cursor, &mut counter);
+    assert_eq!(counter, 23);
+}
+
+#[test]
+fn test_parse_complex_4() {
+    let bytes = "A0016C880162017C3686B18A3D4780".as_bytes();
+    let mut cursor = bytes_to_bin(&bytes);
+    let mut counter = 0;
+    parse(&mut cursor, &mut counter);
+    assert_eq!(counter, 31);
 }
