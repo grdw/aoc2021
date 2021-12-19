d642503980cee96ab311f715e1386d4ee81a3ec5
grdw <gerard@grdw.nl>
2021-12-19 17:59:30 +0100

    Implement the action parser

--- problem_0018/src/graph.rs
+++ problem_0018/src/graph.rs
@@ -28,6 +28,8 @@ fn test_node() {
         Node::leaf(5),
         Node::leaf(2)
     );
+
+    assert_eq!(action(&root), Action::NonAction)
 }
 
 #[test]
@@ -46,4 +48,36 @@ fn test_addition() {
         tree_1,
         tree_2
     );
+
+    assert_eq!(action(&sum), Action::NonAction)
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum Action {
+    Explode,
+    Split,
+    NonAction
+}
+
+fn action(tree: &Node) -> Action {
+    Action::NonAction
+}
+
+#[test]
+fn test_explode() {
+    let mut tree = Node::root(
+        Node::root(
+            Node::root(
+                Node::root(
+                    Node::leaf(0),
+                    Node::leaf(9)
+                ),
+                Node::leaf(2)
+            ),
+            Node::leaf(3)
+        ),
+        Node::leaf(4)
+    );
+
+    assert_eq!(action(&tree), Action::Explode)
 }
